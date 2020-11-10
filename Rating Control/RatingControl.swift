7b9cd023f8cb7986bcecd614c56919c49d305520
Daniel Tull <dt@danieltull.co.uk>
2020-11-10 12:00:29 +0000

    Extract the segments logic to its own struct

--- Rating Control/RatingControl.swift
+++ Rating Control/RatingControl.swift
@@ -12,18 +12,26 @@ struct RatingControl<Value>: View
     @Binding var value: Value
 
     var body: some View {
-        HStack(spacing: 1) {
-            ForEach(Value.allCases) { value in
-                Button(action: { self.value = value }) {
-                    Segment(filled: value <= self.value)
-                }
-            }
-        }
-        .frame(height: 10)
+        Segments(value: $value)
     }
 }
 
 extension RatingControl {
+
+    fileprivate struct Segments: View {
+        @Binding var value: Value
+        var body: some View {
+            HStack(spacing: 1) {
+                ForEach(Value.allCases) { value in
+                    Button(action: { self.value = value }) {
+                        Segment(filled: value <= self.value)
+                    }
+                }
+            }
+            .frame(height: 10)
+        }
+    }
+
     fileprivate struct Segment: View {
         let filled: Bool
         var body: some View {
