7e692ad40d916c7ff5dc9da796f01a84bc95b5ac
Daniel Tull <dt@danieltull.co.uk>
2020-11-10 16:32:25 +0000

    Add accessiblity support

--- Rating Control/RatingControl.swift
+++ Rating Control/RatingControl.swift
@@ -22,6 +22,31 @@ struct RatingControl<Value>: View
 
             Text(value.description)
                 .font(.callout)
+
+        }
+        .accessibilityElement()
+        .accessibilityLabel(title)
+        .accessibilityValue(value.description)
+        .accessibilityAdjustableAction(adjust)
+    }
+}
+
+extension RatingControl {
+
+    private func adjust(direction: AccessibilityAdjustmentDirection) {
+        let values = Value.allCases
+        guard let index = values.firstIndex(of: value) else { return }
+        switch direction {
+        case .increment:
+            let last = values.index(before: values.endIndex)
+            guard let new = values.index(index, offsetBy: 1, limitedBy: last) else { return }
+            value = values[new]
+        case .decrement:
+            let first = values.startIndex
+            guard let new = values.index(index, offsetBy: -1, limitedBy: first) else { return }
+            value = values[new]
+        @unknown default:
+            return
         }
     }
 }
