b8514145d1b9dde3f695c1deec11a97097922cff
Daniel Tull <dt@danieltull.co.uk>
2020-11-10 11:49:12 +0000

    Initial version

---
+++ Rating Control/RatingControl.swift
@@ -0,0 +1,38 @@
+
+import SwiftUI
+
+struct RatingControl: View {
+
+    @State var value: Value = .one
+
+    var body: some View {
+        HStack(spacing: 1) {
+            ForEach(Value.allCases) { value in
+                Button(action: { self.value = value }) {
+                    Segment(filled: value <= self.value)
+                }
+            }
+        }
+        .frame(height: 10)
+    }
+}
+
+extension RatingControl {
+
+    enum Value: CaseIterable, Comparable {
+        case one, two, three, four, five
+    }
+}
+
+extension RatingControl.Value: Identifiable {
+    var id: Self { self }
+}
+
+extension RatingControl {
+    fileprivate struct Segment: View {
+        let filled: Bool
+        var body: some View {
+            filled ? Color.blue : .gray
+        }
+    }
+}
